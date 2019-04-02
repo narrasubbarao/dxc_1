c0d844a564bedb8d0b023ca1d27dd3596e468508
narra012 <narrasubbarao29@gmail.com>
2019-04-02 08:30:10 -1200

    this code changes

--- one.java
+++ one.java
@@ -1,2 +1,6 @@
 this is day one changes from the repository of the 
 cycle to performe of the cycle on the resource
+this code should be writen developer of the commit message
+on the centeral server of the capacity
+this senario of the restrict of vacances of the resource
+
